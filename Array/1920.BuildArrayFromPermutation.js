677ae345944baa0bd15fd232ae61b187bbc5a91e
Boon <boonteck.soh@hotmail.com>
2023-03-22 23:16:56 +0800

    Added Array directory and BuildArrayFromPerm practice

---
+++ Array/1920.BuildArrayFromPermutation.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var buildArray = function(nums) {
+//     let newArray = []
+//     for (let i=0; i < nums.length; i++){
+//         newArray.push(nums[nums[i]])
+//     }
+//     return newArray
+// };
+
+// map function for follow up
+var buildArray = function(nums) {
+    return newArray = nums.map(num => nums[num])
+};
